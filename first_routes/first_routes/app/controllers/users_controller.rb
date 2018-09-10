7c42c3bf195b66be3896df620630cb0c5a2dcd3f
cyip <chris.yip87@gmail.com>
2018-09-10 15:20:14 -0700

    update users_controler.rb

---
+++ first_routes/first_routes/app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def index
+    render plain: "I'm in the index action!"
+  end
+  
+  def create
+    # debugger
+    user = User.new(params.require(:user).permit(:name,:email))
+    if user.save
+      render json: user
+    else 
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+    
+  end 
+  
+  def show 
+    user = User.find(params[:id])
+    if user
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end 
+  end
+end
