019875a4ce19f79b27af3af603c561b66d71287a
cyip <chris.yip87@gmail.com>
2018-09-10 15:36:21 -0700

    Add destroy method to rails API

--- first_routes/first_routes/app/controllers/users_controller.rb
+++ first_routes/first_routes/app/controllers/users_controller.rb
@@ -22,4 +22,22 @@ class UsersController < ApplicationController
       render json: user.errors.full_messages, status: :unprocessable_entity
     end 
   end
+  
+  def update
+    user = User.find(params[:id])
+    if user.update(params.require(:user).permit(:name,:email))
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end 
+  end
+  
+  def destroy
+    user = User.find(params[:id])
+    if user.destroy()
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end 
+  end
 end
