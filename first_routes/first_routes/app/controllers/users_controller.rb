b2a80d6c533200fa301b7957e44204c0f1b0ff9a
cyip <chris.yip87@gmail.com>
2018-09-10 17:14:20 -0700

    Final Commit

--- first_routes/first_routes/app/controllers/users_controller.rb
+++ first_routes/first_routes/app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   
   def destroy
     user = User.find(params[:id])
-    if user.destroy()
+    if user.destroy
       render json: user
     else
       render json: user.errors.full_messages, status: :unprocessable_entity
